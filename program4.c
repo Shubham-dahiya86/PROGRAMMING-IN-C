4c8bbed63f0bb7301a4d6c196ea307e393110b5c
Shubham-dahiya86 <shubhamss1086@gmail.com>
2025-09-03 19:18:04 +0530

    add program 4

---
+++ program4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    printf("Shubham Dahiya\n");
+    printf("367, Sector-104\n");
+    printf("Gurugram, Haryana\n");
+    printf("India - 122006\n");
+    return 0;
+}

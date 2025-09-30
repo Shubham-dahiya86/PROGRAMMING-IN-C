bad7e1db3c44b0a9b342963734a1a5cfe28e8ed2
Shubham-dahiya86 <shubhamss1086@gmail.com>
2025-09-30 11:46:02 +0530

    add program21.c

---
+++ program21.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    int n, factorial = 1, i = 1;
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } 
+    else 
+    {
+        while (i <= n) 
+        {
+            factorial *= i;
+            i++;
+        }
+        printf("Factorial of %d = %d\n", n, factorial);
+    }
+    return 0;
+}
